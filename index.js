8ada5260287ca392e088b9262dc668736357b00c
Irenity <lilithkard77@gmail.com>
2024-12-23 03:18:14 +0600

    more apis for posts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oo5u4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -43,6 +44,34 @@ async function run() {
       app.get('/posts', async (req, res) => {
         const cursor = postCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+      });
+
+      app.get('/posts/:id', async (req, res) => {
+        const id = req.params.id;
+      
+        // Validate if `id` is a valid ObjectId
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send({ error: 'Invalid post ID' });
+        }
+      
+        const query = { _id: new ObjectId(id) };
+        try {
+          const result = await postCollection.findOne(query);
+          if (!result) {
+            return res.status(404).send({ error: 'Post not found' });
+          }
+          res.send(result);
+        } catch (error) {
+          console.error("Error retrieving post:", error);
+          res.status(500).send({ error: "Internal Server Error" });
+        }
+      });
+
+      app.delete('/posts/:id', async (req, res) => {
+        const id = req.params.id; 
+        const query = { _id: new ObjectId(id) };
+        const result = await postCollection.deleteOne(query);
         res.send(result);
       });
 
